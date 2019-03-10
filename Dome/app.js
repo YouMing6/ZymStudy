f31f7b82397b5d8768bfc8b9138536cffab044e9
YouMing6 <42257923+YouMing6@users.noreply.github.com>
2019-03-10 19:11:10 +0800

    Add files via upload

--- Dome/app.js
+++ Dome/app.js
@@ -21,3 +21,29 @@ window.onload = function () {
     },
   })     
 }
+
+function lan(){
+	 // document.getElementsByClassName('conter-list-top')[0].style.background = "url(lan.png) no-repeat center center"  ;
+
+	 var lan = document.getElementsByClassName('conter-list-top')[0];
+	 lan.style.background = "url(lan.png) no-repeat center center";
+
+	 var border1 = document.getElementsByClassName('quan-right')[0];
+	 var border2 = document.getElementsByClassName('quan-left')[0];
+	 border1.style.border = "1px solid #fff";
+	 border2.style.border = "1px solid #666";
+}
+
+function cheng(){
+	// document.getElementsByClassName('conter-list-top')[0].style.background = "url(cheng.png) no-repeat center center"  ;
+
+	var cheng = document.getElementsByClassName('conter-list-top')[0];
+	cheng.style.background = "url(cheng.png) no-repeat center center";
+
+	var border1 = document.getElementsByClassName('quan-left')[0];
+	 var border2 = document.getElementsByClassName('quan-right')[0];
+	 border1.style.border = "1px solid #fff";
+	 border2.style.border = "1px solid #666";
+
+	
+}
